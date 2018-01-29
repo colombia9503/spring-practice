74e45048d173c008b7a454d29265d532f6b354b7
santiago umana <santi950312@gmail.com>
2018-01-29 01:07:35 -0500

    Actualizaciones

--- src/main/java/com/sumana/practica/component/RequestTimeInterceptor.java
+++ src/main/java/com/sumana/practica/component/RequestTimeInterceptor.java
@@ -1,17 +1,31 @@
 package com.sumana.practica.component;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.sumana.practica.repository.LogRepository;
 
 @Component("requestTimeInterceptor")
 public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 	
+	@Autowired
+	@Qualifier("logRepository")
+	private LogRepository logRepository;
+	
 	private static final Log LOG = LogFactory.getLog(RequestTimeInterceptor.class);
+	
+	
 	
 	//antes de la petición
 	@Override
@@ -26,6 +40,15 @@ public class RequestTimeInterceptor extends HandlerInterceptorAdapter {
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
 			throws Exception {
 		long startTime = (long) request.getAttribute("startTime");
+		String url = request.getRequestURL().toString();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String username = "";
+		if(auth != null && auth.isAuthenticated()) {
+			username = auth.getName();
+		}
+		com.sumana.practica.entity.Log log = new com.sumana.practica.entity.Log(new Date(), auth.getDetails().toString(), username, url);
+		logRepository.save(log);
 		LOG.info("Request URL: '" + request.getRequestURL().toString() + "' -- in: '"+(System.currentTimeMillis() - startTime)+"' ms");
+		
 	}
 }
---
+++ src/main/java/com/sumana/practica/repository/LogRepository.java
@@ -0,0 +1,13 @@
+package com.sumana.practica.repository;
+
+import java.io.Serializable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sumana.practica.entity.Log;
+
+@Repository("logRepository")
+public interface LogRepository extends JpaRepository<Log, Serializable> {
+
+}
